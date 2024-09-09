8a2e97e690e9b5b7b0d62ee788babd11abd0c4a7
Дмитрий Кубарев <kubarevds@icloud.com>
2024-09-09 23:40:48 +0300

    fix bugs

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,10 +1,15 @@
 import { ContactsScreen } from "@/components/screens/ContactsScreen";
+import { Breadcrumbs } from "@/components/ui/breadcrumbs/Breadcrumbs";
+import { DASHBOARD_PAGES } from "@/config/url-config/all-pages.config";
 import { NextPage } from "next";
 
 const Contact: NextPage = () => {
-	return (
-		<ContactsScreen />
-	);
+  return (
+    <>
+      <Breadcrumbs pathname={DASHBOARD_PAGES.CONTACTS.url} />
+      <ContactsScreen />
+    </>
+  );
 };
 
 export default Contact;
