6b402642db2c62948315ab2a0abec3b4642ea843
Дмитрий Кубарев <kubarevds@icloud.com>
2024-09-17 12:34:55 +0300

    add tariffs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { METADATA, NO_INDEX_PAGE } from "@/seo/metadata";
 import "@/styles/fonts.css";
 import "@/styles/globals.css";
 import { Metadata } from "next";
+import Script from "next/script";
 import { ReactNode } from "react";
 import { Footer } from "../components/layout/footer/Footer";
 import { Header } from "../components/layout/header/Header";
@@ -16,7 +17,6 @@ export const metadata: Metadata = {
     ...METADATA.HOME,
     siteName: "Make All Perfect",
   },
-
   ...NO_INDEX_PAGE,
 };
 
@@ -27,7 +27,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+
+
       <body suppressHydrationWarning>
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WSL5TXVP');
+          `,
+        }}
+      />
         <div className="page">
           <Providers>
             <Header />
